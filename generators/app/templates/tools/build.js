57a439aee858f7dea6516b4cac51ccc13586ed6f
Iman Mohamadi <iman.mohamadi.dev@gmial.com>
2018-04-12 11:17:04 +0430

    fixes node 7 issues

--- generators/app/templates/tools/build.js
+++ generators/app/templates/tools/build.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const EventEmitter = require('events');
 const rollup = require('rollup');
-const rollupOptions = require('./rollup.config');
 const args = require('easy-argument-parser');
+const async = require('asyncawait/async');
+const await = require('asyncawait/await');
+const rollupOptions = require('./rollup.config');
 
 // see below for details on the options
 const inputOptions = rollupOptions.input;
@@ -13,38 +15,40 @@ var __DEV__ = process.env.NODE_ENVIRONMENT !== 'production';
 
 inputOptions.perf = __DEV__;
 
-async function build() {
-  if (args.watch) {
-    const watcher = rollup.watch(Object.assign(
-      inputOptions,
-      {
-        output: outputOptions,
-        watch: {
-          include: ['src/**', 'node_modules/**'],
-        },
-      },
-    ));
-    watcher.on('event', event => {
-      const nowDateTime = new Date();
-      const now = [nowDateTime.getHours(), nowDateTime.getMinutes(), nowDateTime.getSeconds()].join(':');
-      if (event.code === 'BUNDLE_END') {
-        console.log(now + ' updated in ' + event.duration + 'ms');
+const build = async (
+  function build() {
+    if (args.watch) {
+      const watcher = rollup.watch(Object.assign(
+        inputOptions,
+        {
+          output: outputOptions,
+          watch: {
+            include: ['src/**', 'node_modules/**'],
+          },
+        }
+      ));
+      watcher.on('event', event => {
+        const nowDateTime = new Date();
+        const now = [nowDateTime.getHours(), nowDateTime.getMinutes(), nowDateTime.getSeconds()].join(':');
+        if (event.code === 'BUNDLE_END') {
+          console.log(now + ' updated in ' + event.duration + 'ms');
+        }
+      });
+      // keep watcher alive
+      const emiter = new EventEmitter();
+      emiter.on(Math.random().toString(), () => {});
+    } else {
+      // create a bundle
+      const bundle = await (rollup.rollup(inputOptions));
+      // generate code and a sourcemap
+      const { code, map } = await (bundle.generate(outputOptions));
+      await (bundle.write(outputOptions));
+      // or write the bundle to disk
+      if (__DEV__) {
+        console.log(bundle.getTimings());
       }
-    });
-    // keep watcher alive
-    const emiter = new EventEmitter();
-    emiter.on(Math.random().toString(), () => {});
-  } else {
-    // create a bundle
-    const bundle = await rollup.rollup(inputOptions);
-    // generate code and a sourcemap
-    const { code, map } = await bundle.generate(outputOptions);
-    await bundle.write(outputOptions);
-    // or write the bundle to disk
-    if (__DEV__) {
-      console.log(bundle.getTimings());
     }
   }
-}
+);
 
 build();
